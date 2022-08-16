0e0a491c94dfdd177dc6dfcabee8a233dcdf93a9
Liliana Perez <103470899+o7lili@users.noreply.github.com>
2022-08-16 12:56:13 -0400

    connected server to election/candidates table

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -7,7 +8,22 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // your MySQL username,
+        user: 'root',
+        // your MySQL password
+        password: 'j3rpp=kqhosu@%',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // default response for any other request (not found)
 app.use((req, res) => {
