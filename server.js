3b434945423213c2922db81e51471e472ba3742e
Liliana Perez <103470899+o7lili@users.noreply.github.com>
2022-08-18 14:21:16 -0400

    joined party names and ids with candidates table

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const db = mysql.createConnection(
         // your MySQL username,
         user: 'root',
         // your MySQL password
-        password: 'j3rpp=kqhosu@%',
+        password: '',
         database: 'election'
     },
     console.log('Connected to the election database.')
@@ -25,7 +25,11 @@ const db = mysql.createConnection(
 
 // get all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name
+                    AS party_name
+                    FROM candidates
+                    LEFT JOIN parties
+                    ON candidates.party_id = parties.id`;
 
     db.query(sql, (err, rows) => {
         if (err) {
@@ -41,7 +45,13 @@ app.get('/api/candidates', (req, res) => {
 
 // GET A single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name
+                    AS party_name
+                    FROM candidates
+                    LEFT JOIN parties
+                    ON candidates.party_id = parties.id
+                    WHERE candidates.id = ?`;
+
     const params = [req.params.id];
 
     db.query(sql, params, (err, row) => {
